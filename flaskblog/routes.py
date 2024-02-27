88a6ea47424c8d6540710c8fd945b1dbb5d01fa1
Thomas Durkin <mthomasdurkin@gmail.com>
2024-02-27 11:24:36 -0500

    User Account Page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,12 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
+from PIL import Image
+
+import secrets
+import os
 
 posts = [
 	{
@@ -76,7 +80,42 @@ def logout():
 	logout_user()
 	return redirect(url_for('login'))
 
-@app.route("/account")
+
+def save_picture(picture):
+	random_hex = secrets.token_hex(8)
+	_, f_ext = os.path.splitext(picture.filename)
+	picture_fn = random_hex + f_ext
+	picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+	output_size = (125,125)
+	img = Image.open(picture)
+	img.thumbnail(output_size)
+	img.save(picture_path)
+	
+	return picture_fn
+
+@app.route("/account", methods = ['GET', 'POST'])
 @login_required
 def account():
-	return render_template('account.html', title = 'Account')
+	form = UpdateAccountForm()
+	if form.validate_on_submit():
+		# Update Picture
+		if form.picture.data:
+			picture_file = save_picture(form.picture.data)
+			current_user.img_file = picture_file
+		# Update Username and Email
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Your account has been updated!', 'success')
+		return redirect(url_for('account'))
+	# Auto fill form with current info
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+
+	img_file = url_for('static', filename = 'profile_pics/' + current_user.img_file)
+	return render_template('account.html', 
+							title = 'Account', 
+							image_file = img_file, 
+							form = form)
