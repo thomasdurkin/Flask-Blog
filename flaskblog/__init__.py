da54169d39fc61844d47021723b9ee0dfb651d33
Thomas Durkin <mthomasdurkin@gmail.com>
2024-03-01 13:51:34 -0500

    Password Reset Functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,15 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.app_context().push()
 app.config['SECRET_KEY'] = '6f5371ffabcc0f1c9ff9cb95554336f0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+# Setup environment variables
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+mail = Mail(app)
+
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -17,6 +18,21 @@ class User(db.Model, UserMixin):
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}', '{self.img_file}')"
+	
+	def get_reset_token(self, expires_sec = 1800):
+		s = Serializer(app.config['SECRET_KEY'], expires_sec)
+		return s.dumps({'user_id': self.id}).decode('utf-8')
+	
+	@staticmethod
+	def verify_reset_token(token):
+		s = Serializer(app.config['SECRET_KEY'])
+		try:
+			# Return user_id if it has a valid token
+			# user_id comes from payload
+			user_id = s.loads(token)['user_id']
+		except:
+			return None
+		return User.query.get(user_id)
 	
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
